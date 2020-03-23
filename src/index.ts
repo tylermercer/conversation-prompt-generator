b98dd34949baba971a21b92dc7c3579756701ad5
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-23 00:03:42 -0600

    Continue refining UX and copy text

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { h1, p, span, div, button, blockquote, a } from 'markup-as-js';
-import './styles.css';
+import { h1, p, span, div, button, blockquote, a, footer, small } from 'markup-as-js';
 import 'chota';
+import './styles.css';
 
 import prompts from './prompts';
 
@@ -20,11 +20,19 @@ const getNextPrompt = ():string => {
 const text = span({}, getNextPrompt());
 
 const app = div({ class: "container"},
-  h1({}, "Conversation Promp Generator"),
-  p({}, "In our current state of social isolation in response to COVID-19, many people are having a hard time finding meaningful social interaction. This web app aims to alleviate that by providing questions you can use to prompt meaningful conversations, even over a video call or text message conversation."),
+  h1({}, "Conversation Prompt Generator"),
+  p({}, "In our current state of social isolation in response to COVID-19, many people are experiencing ",
+  a(
+    {
+      href: "https://www.deseret.com/opinion/2020/3/22/21185578/coronavirus-covid-19-side-effect-social-distancing-isolation-quarantine-loneliness", 
+      target: "blank"
+    }, 
+    "profound loneliness"
+  ),
+  ". This web app aims to alleviate that by providing questions to help you have meaningful conversations, even over a video call or text message conversation."),
   p({}, "We recommend that both you and your friend answer the question."),
   blockquote({class: "main-text"}, text),
-  p({},
+  p({class: "row full-width is-center"},
     button(
       {
         class: "button primary",
@@ -36,10 +44,18 @@ const app = div({ class: "container"},
   p({}, 
     "The more prompts you go through, the more they'll require you to disclose. This reciprocal and escalating self-disclosure ", 
     a(
-      {src: "https://journals.sagepub.com/doi/abs/10.1177/0146167297234003"}, 
+      {href: "https://journals.sagepub.com/doi/abs/10.1177/0146167297234003", target: "blank"}, 
       "has been shown"
     ), 
-    " to lead to a feeling of closeness between conversational partners.")
+    " to lead to a feeling of closeness between conversational partners."
+  ),
+  footer({},
+    small({class: "text-grey"},
+      "Built with 💜 by ",
+      a({href:"https://github.com/tylermercer", target: "blank"}, "Tyler Mercer"),
+      ".",
+    ),
+  ),
 );
 
 document.getElementById("app")?.appendChild(app);
