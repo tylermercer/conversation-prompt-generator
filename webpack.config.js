9c93ca13d587820ffb99d4c21c312fad3e8fd928
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-22 21:47:11 -0600

    Initial create-app project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.ts(x)?$/,
+        use: [
+          'awesome-typescript-loader'
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  }
+};
+
+module.exports = config;
