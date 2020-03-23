21e39ec56eaf5b5e9ae96d7a3fe88bbe87e1bb42
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-22 23:32:24 -0600

    Create basic functionality, with a few questions.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("hello world!");
+import { h1, p, span, div, button, blockquote } from 'markup-as-js';
+import './styles.css';
+import 'chota';
+
+import prompts from './prompts';
+
+// const icongram = (icon: string) => `https://icongr.am/simple/${icon}.svg`;
+
+var remainingPrompts: string[] = [...prompts];
+var usedPrompts: string[] = [];
+
+const getNextPrompt = ():string => {
+  let index = Math.floor(Math.random() * Math.min(remainingPrompts.length, 5));
+  let prompt = remainingPrompts[index];
+  remainingPrompts = remainingPrompts.length > 1 ? [...remainingPrompts.slice(0, index), ...remainingPrompts.slice(index + 1)] : [...prompts];
+  usedPrompts = [...usedPrompts, prompt];
+  return prompt;
+}
+
+const text = span({}, getNextPrompt());
+
+const app = div({ class: "container"},
+  h1({}, "Conversation Prompts"),
+  p({}, "In our current state of social isolation in response to COVID-19, many people are having a hard time finding meaningful social interaction. This web app aims to alleviate that by providing questions you can use to prompt meaningful conversations, even over a video call or text message conversation."),
+  blockquote({class: "main-text"}, text),
+  div({},
+    button(
+      {
+        class: "button primary",
+        onclick: () => text.innerText = getNextPrompt()
+      }, 
+      "New Prompt"
+    ),
+  ),
+);
+
+document.getElementById("app")?.appendChild(app);
---
+++ src/prompts.ts
@@ -0,0 +1,11 @@
+const prompts = [
+  "Given the choice of anyone in the world, who would you want as a dinner guest?",
+  "Would you like to be famous? In what way?",
+  "Before making a phone call, do you ever rehearse what you're going to say? Why?",
+  "What would constitute a perfect day for you?",
+  "When did you last sing to yourself? To someone else?",
+  "If you were able to live to the age of 90 and retain either the mind or body of a 30-year old for the last 60 years of your life, which would you choose?",
+  "Do you have a secret hunch about how you will die?",
+];
+
+export default prompts;
