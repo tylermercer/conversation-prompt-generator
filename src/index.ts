ecbc6101ec8b73febd3066dcf41e8fbd5a86a943
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-22 23:46:45 -0600

    Improve copy text

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { h1, p, span, div, button, blockquote } from 'markup-as-js';
+import { h1, p, span, div, button, blockquote, a } from 'markup-as-js';
 import './styles.css';
 import 'chota';
 
@@ -22,8 +22,9 @@ const text = span({}, getNextPrompt());
 const app = div({ class: "container"},
   h1({}, "Conversation Prompts"),
   p({}, "In our current state of social isolation in response to COVID-19, many people are having a hard time finding meaningful social interaction. This web app aims to alleviate that by providing questions you can use to prompt meaningful conversations, even over a video call or text message conversation."),
+  p({}, "We recommend that both you and your friend answer the question."),
   blockquote({class: "main-text"}, text),
-  div({},
+  p({},
     button(
       {
         class: "button primary",
@@ -32,6 +33,13 @@ const app = div({ class: "container"},
       "New Prompt"
     ),
   ),
+  p({}, 
+    "The more prompts you go through, the more they'll require you to disclose. This reciprocal and escalating self-disclosure ", 
+    a(
+      {src: "https://journals.sagepub.com/doi/abs/10.1177/0146167297234003"}, 
+      "has been shown"
+    ), 
+    " to lead to a feeling of closeness between conversational partners.")
 );
 
 document.getElementById("app")?.appendChild(app);
